0f87a7880f25c0f7b3e73b72b69f840c5b484b25
Atul Bagga <atbagga@microsoft.com>
2020-01-22 14:30:22 +0530

    Remove workflow yaml

--- HelloWorld/__init__.py
+++ HelloWorld/__init__.py
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello {name}! Welcome to FunctionApp Tutorial. (PREVIEW MODE)")
+        return func.HttpResponse(f"Hello {name}! Welcome to FunctionApp Tutorial. Update 1.")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
